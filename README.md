73196dae38dd2abc8e13df077c0bf4fa37e6222e
ryohey <info@codingcafe.jp>
2022-09-29 14:05:55 +0900

    Update

---
+++ README.md
@@ -0,0 +1,23 @@
+# migrate-swiftgen-l10n
+
+Replace `NSLocalizedString("foobar.buzz")` to `L10n.FooBar.buzz`
+
+## Usage
+
+```sh
+npm install -g migrate-swiftgen-l10n
+```
+
+```sh
+npx migrate-swiftgen-l10n path/to/source/dir
+```
+
+## Option
+
+### --dry-run
+
+Use the --dry-run option to see what is replaced without changing the file.
+
+```sh
+npx migrate-swiftgen-l10n path/to/source/dir --dry-run
+```
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import recursive from "recursive-readdir"
 function convertKey(str) {
   // 事前に処理
   const cleanStr = str
-    .replaceAll(/[,:\/%@]/gm, " ") // 対応できない文字はスペースにする
+    .replaceAll(/[,:\/%@\(\)\?\!]/gm, " ") // 対応できない文字はスペースにする
     .replaceAll(/\.+/gm, ".") // . の連続はひとつの . に
     .replace(/\.$/, "") // 末尾の . を消す
 
@@ -26,34 +26,79 @@ function convertKey(str) {
   return [last2, ...tokens2].reverse().join(".")
 }
 
+function convertKeyObjC(str) {
+  return convertKey(str.replaceAll(/\./gm, " ")) // . でネストしない
+}
+
+const insertAt = (str, sub, pos) =>
+  `${str.slice(0, pos)}${sub}${str.slice(pos)}`
+
 const targetPath = process.argv[2]
-console.log(targetPath)
-const files = await recursive(targetPath, [
+const isDryRun = process.argv[3] === "--dry-run"
+
+const swiftFiles = await recursive(targetPath, [
   (file, stats) => !stats.isDirectory() && !file.endsWith(".swift"),
 ])
 
-await Promise.all(
-  files.map(async (file) => {
-    const text = await fs.readFile(file, "utf-8")
-    const newText = text
-      .replaceAll(
-        /String\(format: NSLocalizedString\("(.+?)", comment: ".*?"\), (.+?)\)/gm,
-        (match, p1, p2) => {
-          const key = convertKey(p1)
-          const result = `L10n.${key}(${p2})`
-          console.log(`Replace ${match} to ${result}`)
-          return result
-        }
-      )
-      .replaceAll(
-        /NSLocalizedString\("(.+?)", comment: ".*?"\)/gm,
-        (match, p1) => {
-          const key = convertKey(p1)
-          const result = `L10n.${key}`
-          console.log(`Replace ${match} to ${result}`)
-          return result
-        }
-      )
+for await (const file of swiftFiles) {
+  const text = await fs.readFile(file, "utf-8")
+  const newText = text
+    .replaceAll(
+      /String\(format: NSLocalizedString\("([^"]+?)", comment: ".*?"\), (.+?)\)/gm,
+      (match, p1, p2) => {
+        const key = convertKey(p1)
+        const result = `L10n.${key}(${p2})`
+        console.log(`Replace ${match} to ${result}`)
+        return result
+      }
+    )
+    .replaceAll(
+      /NSLocalizedString\("([^"]+?)", comment: ".*?"\)/gm,
+      (match, p1) => {
+        const key = convertKey(p1)
+        const result = `L10n.${key}`
+        console.log(`Replace ${match} to ${result}`)
+        return result
+      }
+    )
+  if (!isDryRun) {
     await fs.writeFile(file, newText, "utf-8")
-  })
-)
+  }
+}
+
+const objcFiles = await recursive(targetPath, [
+  (file, stats) => !stats.isDirectory() && !file.endsWith(".m"),
+])
+
+for await (const file of objcFiles) {
+  const text = await fs.readFile(file, "utf-8")
+  let newText = text
+    .replaceAll(
+      /\[NSString stringWithFormat:NSLocalizedString\(@"([^"]+?)",.+?\), (.+?)\]/gm,
+      (match, p1, p2) => {
+        const key = convertKeyObjC(p1)
+        const result = `[Localizable ${key}WithValue:${p2}]`
+        console.log(`Replace ${match} to ${result}`)
+        return result
+      }
+    )
+    .replaceAll(/NSLocalizedString\(@"([^"]+?)", .+?\)/gm, (match, p1) => {
+      const key = convertKeyObjC(p1)
+      const result = `Localizable.${key}`
+      console.log(`Replace ${match} to ${result}`)
+      return result
+    })
+  // Add #import "Localizable.h" if needed
+  if (newText != text && !newText.includes(`#import "Localizable.h"`)) {
+    let matches = Array.from(newText.matchAll(/^#import .+$/gm))
+    let lastMatch = matches[matches.length - 1]
+    newText = insertAt(
+      newText,
+      `\n#import "Localizable.h"`,
+      lastMatch.index + lastMatch[0].length
+    )
+  }
+  if (!isDryRun) {
+    await fs.writeFile(file, newText, "utf-8")
+  }
+}
